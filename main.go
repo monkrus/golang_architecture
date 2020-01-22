140bc296e504a799a86949c49f23c18cae0d3151
Sergei Stadnik <sergeisqa@gmail.com>
2020-01-21 20:29:23 -0700

    second example of interfaces

--- main.go
+++ main.go
@@ -3,39 +3,52 @@ package main
 
 import "fmt"
 
+//concrete type person
 type person struct {
-
 first string
 }
 
-
-
 func (p person) speak() {
-
-fmt.Println ("from a person - this is my name", p.first)
+fmt.Println ("My last warning, ", p.first)
 }
 
+
+type secretAgent struct {
+person
+ltk bool	
+}
+
+func (sa secretAgent) speak() {
+fmt.Println ("Run now, " , sa.first)
+}
 
 // any TYPE that has the methods specified by an interface
 // is also of the interface type
 // an interface says
 // "Hey, baby, if you have these methods, then you`re my type."
 
-
 type human interface {
-	speak()
+speak()
 }
-
 
 func main() {
 
-		p1 := person{
-			first:"James",
+//concrete type is person, secretAgent
+//ABSTRACT type is human
+p1 := person{ 
+	first:"Miss Moneypenny",
+}
+
+sa1 := secretAgent {
+	person: person {
+		first:"James",	
+	},
+	ltk: true,
+}
 			
-		}
 
 	fmt.Printf("%T\n", p1)
-}
+
 
 //1. Type person created
 //2. Attached a method speak() with receiver (p person)
@@ -44,6 +57,9 @@ func main() {
 //   p1 is both TYPE person and TYPE human
 
 
-var x human
+var x, y human
 x = p1
-fmt.Printf("%T\n", x)
+y = sa1
+x.speak()
+y.speak()
+}

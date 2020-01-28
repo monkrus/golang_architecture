bdf323a24825bb7fee3d91ea185790ba6877b037
Sergei Stadnik <sergeisqa@gmail.com>
2020-01-28 13:08:35 -0700

    Goroutine with cancel example

--- main.go
+++ main.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
-//PARENT context
+	fmt.Printf("GOROUTINES RUNNING %d\n", runtime.NumGoroutine())
+	//PARENT context
 	ctx := context.Background()
-// CHILD Context with timeout returns two values
-//waits for half second
-	ctx, cancelF := context.WithTimeout(ctx, 500*time.Millisecond)
-// cleanup, shutting it down
+	// CHILD Context with timeout returns two values
+	ctx, cancelF := context.WithCancel(ctx)
+	// cleanup, shutting it down
 	defer cancelF()
-// then some work starts
-	time.Sleep(100* time.Millisecond)
-//check the status of the channel
 
-//when done channel gets closed it is in `"false" and has a ZERO value.`
-//what is coming off the channel?
-select {
+	// start a bunch of goroutines
+	for i := 0; i < 100; i++ {
+		// call an anonymous func
+		go func(n int) {
+			fmt.Println("launching goroutine", n)
+			for {
+				select {
+				case <-ctx.Done():
+					runtime.Goexit()
+				// `return` is an alternative to `Goexit`
+				//return
+				default:
+					fmt.Printf("goroutine %d is doing work\n", n)
+					time.Sleep(50 * time.Millisecond)
+				}
+			}
+		}(i)
 
-case <-ctx.Done();
-//not finished because done returned a VALUE !
-	fmt.Printl("work did not finished")
-default:
-	fmt.Printl("work  finished")
 	}
+	time.Sleep(time.Millisecond)
+	fmt.Printf("GOROUTINES RUNNING AFTER ONE MILLISECOND %d\n", runtime.NumGoroutine())
+	cancelF()
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("GOROUTINES RUNNING AFTER Cancelfunc called %d\n", runtime.NumGoroutine())
 
 }
-
